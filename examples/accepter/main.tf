d52492d30f5908a3efc708f18f4d5ee84a15351b
aashari <mq.aashari@gmail.com>
2020-05-19 11:03:52 +0700

    Add import.sh to help import existing VPC peering configuration and update readme

--- examples/accepter/main.tf
+++ examples/accepter/main.tf
@@ -10,21 +10,21 @@ provider "aws" {
   region  = "ap-southeast-1"
 }
 
-module "vpc_peering_to_requester" {
+module "vpc_peering_accepter" {
   source = "../../"
 
   product_domain = "tsi"
   environment    = "testing"
 
   is_requester               = "false"
-  vpc_id                     = "vpc-01c234ebd567af890"
-  accepter_account_alias     = "tvlk-tsi-dev"
-  requester_account_alias    = "tvlk-bei-dev"
-  vpc_peering_connection_id  = "pcx-1a2b3c4d5e6f7a890"
+  vpc_id                     = "vpc-1a2b3c4d5e6f7g8h9i"
+  accepter_account_alias     = "aws-accepter"
+  requester_account_alias    = "aws-requester"
+  vpc_peering_connection_id  = "pcx-1a2b3c4d5e6f7g8h9i"
   destination_vpc_cidr_block = "192.166.0.0/16"
-  peer_account_id            = ""
-  peer_vpc_id                = ""
-  peer_vpc_region            = ""
+  peer_account_id            = "12345678913"
+  peer_vpc_id                = "vpc-2a2b3c4d5e6f7g8h9i"
+  peer_vpc_region            = "ap-southeast-1"
 
   is_connection_accepted = "true"
 }
--- examples/requester/main.tf
+++ examples/requester/main.tf
@@ -10,20 +10,20 @@ provider "aws" {
   region  = "ap-southeast-1"
 }
 
-module "vpc_peering_to_accepter" {
+module "vpc_peering_requester" {
   source = "../../"
 
   product_domain = "bei"
   environment    = "testing"
 
   is_requester               = "true"
-  vpc_id                     = "vpc-1234a5f6e7d8c9b0a"
-  accepter_account_alias     = "tvlk-tsi-dev"
-  requester_account_alias    = "tvlk-bei-dev"
+  vpc_id                     = "vpc-2a2b3c4d5e6f7g8h9i"
+  accepter_account_alias     = "aws-accepter"
+  requester_account_alias    = "aws-requester"
   vpc_peering_connection_id  = ""
   destination_vpc_cidr_block = "192.164.0.0/16"
-  peer_account_id            = "285292138376"
-  peer_vpc_id                = "vpc-01c234ebd567af890"
+  peer_account_id            = "12345678912"
+  peer_vpc_id                = "vpc-1a2b3c4d5e6f7g8h9i"
   peer_vpc_region            = "ap-southeast-1"
 
   is_connection_accepted = "false" # change this to true after the connection is accepted by accepter and do terraform apply again
