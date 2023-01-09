73478b466ff0e31ad4c9c94ee6ccfc5b7baa650e
ariansyah <ariansyahy@yahoo.com>
2023-01-09 19:01:21 +0700

    revise the deprecated map

--- main.tf
+++ main.tf
@@ -61,8 +61,8 @@ resource "aws_vpc_peering_connection_accepter" "accepter" {
 
   tags = merge(
     var.additional_tags,
-    tomap({"Name" = format("%s-%s", var.requester_account_alias, var.accepter_account_alias)}),
-    tomap({"ProductDomain" = var.product_domain}),
+    #tomap({"Name" = format("%s-%s", var.requester_account_alias, var.accepter_account_alias)}),
+    #tomap({"ProductDomain" = var.product_domain}),
     #map("Environment", var.environment),
     #map("Description", format("VPC peering connection to %s", var.requester_account_alias)),
     #map("ManagedBy", "terraform"),
